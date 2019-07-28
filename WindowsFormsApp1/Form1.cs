9119b5bd1d1b449e898bc7549e90425ffe3703d9
norx <norx@qq.com>
2019-07-28 22:57:04 +0800

    the 3rd commit : very good

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -25,5 +25,10 @@ namespace WindowsFormsApp1
         {
             MessageBox.Show("Hello everyone !");
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Very Good !");
+        }
     }
 }
